0db64e12faca605828f4f01c05f4496e5256816e
Khalil Hamani <hamani_khalil@yahoo.fr>
2026-02-01 12:54:26 +0100

    fix: applied a working plugin for maven central publication

--- core/build.gradle.kts
+++ core/build.gradle.kts
@@ -2,7 +2,6 @@
 
 import org.jetbrains.kotlin.gradle.ExperimentalWasmDsl
 import org.jetbrains.kotlin.gradle.targets.js.dsl.ExperimentalDistributionDsl
-import org.jetbrains.kotlin.gradle.dsl.KotlinMultiplatformExtension
 
 plugins {
     alias(libs.plugins.jetbrains.kotlin.multiplatform)
@@ -12,12 +11,48 @@ plugins {
     alias(libs.plugins.kover)
     alias(libs.plugins.sonar)
     alias(libs.plugins.dokka)
+    alias(libs.plugins.publish)
+    signing
 }
 
 group = rootProject.group
 version = rootProject.version
 
-apply("../gradle/publish.gradle.kts")
+mavenPublishing {
+    publishToMavenCentral()
+
+    signAllPublications()
+
+    coordinates(group.toString(), "stepper-core", version.toString())
+    pom {
+        name.set("stepper")
+        description.set("A simple stepper library for Compose Multiplatform.")
+        url.set("https://github.com/n-apos/stepper")
+        licenses {
+            license {
+                name.set("The Apache License, Version 2.0")
+                url.set("http://www.apache.org/licenses/LICENSE-2.0.txt")
+            }
+        }
+        developers {
+            developer {
+                id.set("n-apos")
+                name.set("N-APOS")
+                email.set("contact@n-apos.com")
+            }
+        }
+        scm {
+            connection.set("scm:git:github.com/n-apos/stepper.git")
+            developerConnection.set("scm:git:ssh://github.com/n-apos/stepper.git")
+            url.set("https://github.com/n-apos/stepper/tree/main")
+        }
+    }
+}
+
+configure<SigningExtension> {
+    useGpgCmd()
+    sign(extensions.getByType(PublishingExtension::class.java).publications)
+}
 
 kotlin {
     listOf(
